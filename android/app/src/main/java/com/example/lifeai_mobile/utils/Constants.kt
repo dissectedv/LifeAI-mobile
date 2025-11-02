f072d86896948938363cb693d4a877b0519cb9ab
Guilherme Silva <guilhermesilvarosa92@gmail.com>
2025-11-02 11:33:49 -0400

    actions para backend

--- android/app/src/main/java/com/example/lifeai_mobile/utils/Constants.kt
+++ android/app/src/main/java/com/example/lifeai_mobile/utils/Constants.kt
@@ -2,5 +2,6 @@ package com.example.lifeai_mobile.utils
 
 object Constants {
 //    const val BASE_URL = "http://18.188.47.129:8000/" instancia do victor
-    const val BASE_URL = "http://192.168.0.12:8000/" // meu ip k8s local
+//    const val BASE_URL = "http://192.168.0.12:8000/" // meu ip k8s local
+    const val BASE_URL = "http://18.117.17.104:8000/" // instancia do gui
 }
