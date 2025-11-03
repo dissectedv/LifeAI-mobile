51a861af9daf1f7c71678cb25ea50d412eefe633
dissectedv <joaovictorsampaio393@gmail.com>
2025-11-03 17:53:42 -0400

    Changes

--- android/app/src/main/java/com/example/lifeai_mobile/utils/Constants.kt
+++ android/app/src/main/java/com/example/lifeai_mobile/utils/Constants.kt
@@ -2,5 +2,5 @@ package com.example.lifeai_mobile.utils
 
 object Constants {
     // Backend Django rodando na instância AWS
-    const val BASE_URL = "http://3.14.126.94:30540/"
+    const val BASE_URL = "http://3.128.194.210:30540/"
 }
