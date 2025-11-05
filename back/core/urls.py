8895d21bc0fd55f531f2c4434f61cdba6ef9704a
dissectedv <joaovictorsampaio393@gmail.com>
2025-11-05 10:13:43 -0400

    corrige backend

--- back/core/urls.py
+++ back/core/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from core import viewsets
 from core import views
-from core.services.api_ia import chat_ia_view
 from .services import api_ia
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -13,7 +12,6 @@ urlpatterns = [
     path('imc/historico/', viewsets.DesempenhoImc.as_view(), name='desempenho-imc'),
     path('imc/registrosConsultas/', viewsets.RegistrosImc.as_view(), name='registros_imc'),
     path('imc/<int:pk>/', viewsets.ImcDeleteAPIView.as_view(), name='deletar_imc'),
-    path('chat-ia/', chat_ia_view, name="chat-ia"),
     path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('imc_base_perfil/', viewsets.ImcBaseAPIView.as_view(), name='imc_base_perfil'),
@@ -26,6 +24,8 @@ urlpatterns = [
     path('pontuacoes/', viewsets.PontuacaoCheckListAPIView.as_view(), name='pontuacoes'),
     path('pontuacoes/mensal/', viewsets.DesempenhoMensalAPIView.as_view(), name='pontuacoes-mensal'),
     path("health/", views.health_check, name="health_check"),
+    
+    # Estas são as linhas corretas que você já tem
     path('chat-ia/', api_ia.chat_ia_view, name='chat-ia'),
     path('gerar-dieta-ia/', api_ia.gerar_dieta_ia_view, name='gerar-dieta-ia'),
 ]
