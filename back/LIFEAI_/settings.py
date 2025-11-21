a214ad26fe11cef82089e6b12eb18ba874c374b9
Guilherme Rosa <guilherme.rosa@fpf.br>
2025-11-20 22:49:51 -0300

    envio de email para novo user

--- back/LIFEAI_/settings.py
+++ back/LIFEAI_/settings.py
@@ -49,9 +49,9 @@ EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp-relay.brevo.com')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True').lower() in ('true', '1')
 EMAIL_USE_SSL = False
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'contatolifeai@gmail.com')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
 # ======== DATABASE ========
 DATABASES = {
